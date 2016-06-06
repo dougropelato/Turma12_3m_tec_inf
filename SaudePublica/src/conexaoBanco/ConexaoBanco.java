89e0ae3bbc44c801ebd3d7c696c4a73f043730e1
FernandoLuizPasquali <Fernandoqualquerbosta@gmail.com>
2016-06-06 20:51:20 -0300

    Mudar nome banco

--- SaudePublica/src/conexaoBanco/ConexaoBanco.java
+++ SaudePublica/src/conexaoBanco/ConexaoBanco.java
@@ -14,7 +14,7 @@ public class ConexaoBanco {
     public  static Connection getConexao() throws ClassNotFoundException, SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://192.168.1.254/saudepublica", "teste", "");
+            return DriverManager.getConnection("jdbc:mysql://192.168.1.254/saude_publica", "teste", "");
         }catch(ClassNotFoundException e){
             throw new SQLException(e.getMessage());
         }
