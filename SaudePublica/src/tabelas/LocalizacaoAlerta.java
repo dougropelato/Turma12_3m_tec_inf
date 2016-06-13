4497a76a5a1018f0a9d33df51681780c668bf2e6
ThiagoAndreazza <thiagoandreazzata@gmail.com>
2016-06-13 20:31:14 -0300

    Localizacões
    Localização alerta e unidades de saude

---
+++ SaudePublica/src/tabelas/LocalizacaoAlerta.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabelas;
+
+/**
+ *
+ * @author Micro06
+ */
+public class LocalizacaoAlerta {
+    private int cord_lat;
+    private int cord_long;
+    private int cod_localizacao;
+    private int cod_alerta;
+
+    /**
+     * @return the cord_lat
+     */
+    public int getCord_lat() {
+        return cord_lat;
+    }
+
+    /**
+     * @param cord_lat the cord_lat to set
+     */
+    public void setCord_lat(int cord_lat) {
+        this.cord_lat = cord_lat;
+    }
+
+    /**
+     * @return the cord_long
+     */
+    public int getCord_long() {
+        return cord_long;
+    }
+
+    /**
+     * @param cord_long the cord_long to set
+     */
+    public void setCord_long(int cord_long) {
+        this.cord_long = cord_long;
+    }
+
+    /**
+     * @return the cod_localizacao
+     */
+    public int getCod_localizacao() {
+        return cod_localizacao;
+    }
+
+    /**
+     * @param cod_localizacao the cod_localizacao to set
+     */
+    public void setCod_localizacao(int cod_localizacao) {
+        this.cod_localizacao = cod_localizacao;
+    }
+
+    /**
+     * @return the cod_alerta
+     */
+    public int getCod_alerta() {
+        return cod_alerta;
+    }
+
+    /**
+     * @param cod_alerta the cod_alerta to set
+     */
+    public void setCod_alerta(int cod_alerta) {
+        this.cod_alerta = cod_alerta;
+    }
+}
